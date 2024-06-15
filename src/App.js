a31bb46353f0073e8784a399b13908d987fafe38
Rahul <chrahulofficial@gmail.com>
2024-06-15 21:21:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { HeroSection } from "./components/index";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#222222] w-screen h-screen">
+      <HeroSection />
+
+      <div className="flex justify-center mt-16">
+        <div className="items-center justify-around max-w-md gap-20 space-y-3 text-white md:max-w-7xl md:flex">
+          <h1 className="text-lg font-bold md:text-5xl">What We Do</h1>
+          <div className="space-y-5 md:max-w-2xl">
+            <p className="font-semibold md:text-2xl">
+              We are a website agency covering high-level Design, Development,
+              and SEO.
+            </p>
+            <p className="font-semibold md:text-2xl">
+              Our website packages are diverse from stunning landing pages to
+              complex website apps.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IoArrowForward } from "react-icons/io5";
+
+export default function HeroSection() {
+  return (
+    <div>
+      <div className="py-10 space-y-4 text-center text-white">
+        <h1 className="text-6xl font-bold px-">SAAS STUDIOS</h1>
+        <p className="max-w-sm m-auto text-lg font-semibold">
+          WE DESIGN AND DEVELOP HIGH-LEVEL DIGITAL PRODUCTS.
+        </p>
+      </div>
+      <div>
+        <button className="flex items-center justify-center space-x-3 bg-[#aaeec4] w-[85vw] md:w-[70vw] lg:w-[20vw] mx-auto py-3.5 rounded-full ">
+          <h1 className="text-xl font-semibold ">Book a Call</h1>
+          <IoArrowForward size={25} className="-rotate-45" />
+        </button>
+      </div>
+    </div>
+  );
+}
