65000e25a44e47b9d0ffa1eeeba6983f33640672
Rahul <chrahulofficial@gmail.com>
2024-06-15 23:56:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -139,6 +139,31 @@ export default function App() {
           );
         })}
       </div>
+
+      <footer className="pb-10 overflow-hidden">
+        <div className="flex animate-marquee whitespace-nowrap">
+          <h1 className="mr-16 text-6xl font-bold text-white md:text-9xl">
+            SAAS STUDIOS INDIA
+          </h1>
+          <h1 className="mr-16 text-6xl font-bold text-white md:text-9xl">
+            SAAS STUDIOS INDIA
+          </h1>
+          <h1 className="text-6xl font-bold text-white md:text-9xl">
+            SAAS STUDIOS INDIA
+          </h1>
+        </div>
+        <div className="flex animate-marquee2 whitespace-nowrap">
+          <h1 className="mr-16 text-6xl font-bold text-white md:text-9xl">
+            SAAS STUDIOS INDIA
+          </h1>
+          <h1 className="mr-16 text-6xl font-bold text-white md:text-9xl">
+            SAAS STUDIOS INDIA
+          </h1>
+          <h1 className="text-6xl font-bold text-white md:text-9xl">
+            SAAS STUDIOS INDIA
+          </h1>
+        </div>
+      </footer>
     </div>
   );
 }
