7c6c2d62dd1bfe6c75fe4fa4feb968690b05ece1
Rahul <chrahulofficial@gmail.com>
2024-06-17 13:25:09 +0530

    dn

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeroSection, Navbar } from "../components";
 import Footer from "../components/Footer";
 
 export default function Contact() {
+  const [data, setdata] = useState({
+    Name: "",
+    Email: "",
+    Message: "",
+  });
+
+  const saveData = () => {
+    console.log(data);
+  };
+
   return (
-    <div className="bg-[#222222] w-screen  overflow-x-clip p-5">
+    <div className="bg-[#222222] w-screen  overflow-x-clip px-2.5">
       <HeroSection tittle={"CONTACT US"} message={"we will contact you "} />
-      <div className="border-[1px] border-stone-700 shadow-xl max-w-6xl px-10 py-8 mx-auto mb-20 text-white rounded-lg  md:max-w-5xl lg:max-w-3xl">
+      <div className="border-[1px] border-stone-700 shadow-xl max-w-7xl px-10 py-8 mx-auto mb-20 text-white rounded-lg  md:max-w-5xl lg:max-w-3xl">
         <div className="space-y-2 text-center">
           <h1 className="text-2xl font-bold md:text-5xl">Contact Us</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -17,6 +27,10 @@ export default function Contact() {
             <input
               type="text"
               placeholder="Name"
+              value={data.Name}
+              onChange={(e) => {
+                setdata({ ...data, Name: e.target.value });
+              }}
               className="text-white w-full py-3.5 px-2 rounded-lg outline-none bg-zinc-800"
             />
           </div>
@@ -25,15 +39,29 @@ export default function Contact() {
             <input
               type="text"
               placeholder="Email"
+              value={data.Email}
+              onChange={(e) => {
+                setdata({ ...data, Email: e.target.value });
+              }}
               className="text-white w-full py-3.5 px-2 rounded-lg outline-none bg-zinc-800"
             />
           </div>
           <div className="w-full space-y-3.5">
             <h1 className="text-white">Message</h1>
             <textarea
-              className="w-full py-3.5 px-2 rounded-lg outline-none bg-zinc-800"
+              className="w-full py-3.5 px-2 rounded-lg outline-none bg-zinc-800 text-white"
               placeholder="Message"
+              value={data.Message}
+              onChange={(e) => {
+                setdata({ ...data, Message: e.target.value });
+              }}
             ></textarea>
+            <button
+              onClick={saveData}
+              className="w-full text-center bg-[#aaeec4] py-3 rounded-lg my-2 font-semibold"
+            >
+              Submit
+            </button>
           </div>
         </div>
       </div>
